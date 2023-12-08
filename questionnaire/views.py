cd82eff047f5de4c634cb312c8d94a0e20e309f1
Felipe Seiberlich <philzaiber@outlook.com>
2023-12-08 09:02:53 +0000

    Link front-end to the back-end

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def get_questionnaire_questions(request):
-    return render(request, 'questionnaire/questionnaire_questions.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'questionnaire/questionnaire_questions.html', context)
