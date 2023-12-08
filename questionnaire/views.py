4fdffaf6d41b67ab5fb5fa647d5fa8eae2d73bd7
Felipe Seiberlich <philzaiber@outlook.com>
2023-12-08 08:54:48 +0000

    Add space above class definition

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def get_questionnaire_questions(request):
     return render(request, 'questionnaire/questionnaire_questions.html')
