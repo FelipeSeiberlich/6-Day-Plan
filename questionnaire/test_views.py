da4dbb9838c68f3aa0d5400bac97f493da8b4f16
Felipe Seiberlich <philzaiber@outlook.com>
2023-12-11 14:42:44 +0000

    Add 3 tests for the forms

--- questionnaire/test_views.py
+++ questionnaire/test_views.py
@@ -7,11 +7,4 @@ class TestDjango(TestCase):
     def test_workability(self):
         self.assertEqual(1, 1)
 
-    def test_workability2(self):
-        self.assertEqual(1, 3)
     
-    def test_workability3(self):
-        self.assertEqual(1, )
-    
-    def test_workability4(self):
-        self.assertEqual(1, 4)
